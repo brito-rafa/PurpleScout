b8b3d802a56e6f853385ae25f148e94c7a717826
Gabe Tao <tao.gabe@gmail.com>
2023-08-27 21:47:03 -0500

    form + post request

--- main.py
+++ main.py
@@ -18,6 +18,19 @@ def home():
 def scout():
     return render_template('scout.html')
 
+# Submit
+@app.route('/submit.html', methods=['GET', 'POST'])
+def submit():
+    print(f"got request via {request.method}")
+    if request.method == 'POST':
+        # print the data recieved from the form
+        for key, value in request.form.items():
+            print(f"{key}: {value}")
+            # we could save the data to a file here
+
+            
+    return render_template('submit.html')
+
 # Run app  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
