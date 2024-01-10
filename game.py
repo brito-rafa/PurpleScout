d49d2f8199a6e537c6da98f83f22fa0eff6d811f
Gabe Tao <tao.gabe@gmail.com>
2024-01-09 22:06:03 -0600

    Update for crescendo form

--- game.py
+++ game.py
@@ -3,7 +3,7 @@
 # Below is an example from Charged Up 2023.
 
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, RadioField, TextAreaField
+from wtforms import IntegerField, RadioField, TextAreaField, BooleanField, SelectField
 from wtforms.validators import *
 
 class ChargedUpScoutForm(FlaskForm):
@@ -63,9 +63,44 @@ class UltimateAscentScoutForm(FlaskForm):
 class CrescendoForm(FlaskForm):
     name = "Crescendo 2024"
 
+    # Basic match and team information
     matchNum = IntegerField('matchNumber', validators=[DataRequired()])
     teamNum = IntegerField('teamNumber', validators=[DataRequired()])
 
-    ### Insert new fields here ###
+    # Autonomous (15 seconds)
+    autoMobility = BooleanField('autoMobility', default=False)
+    autoAmp = IntegerField('autoAmp', validators=[DataRequired()], default=0)
+    autoSpeaker = IntegerField('autoSpeaker', validators=[DataRequired()], default=0)
+
+    # Teleoperated (135 seconds)
+    teleopAmp = IntegerField('teleopAmp', validators=[DataRequired()], default=0)
+    teleopSpeaker = IntegerField('teleopSpeaker', validators=[DataRequired()], default=0)
+    teleopAmplified = IntegerField('teleopAmplified', validators=[DataRequired()], default=0)
+
+    # Endgame
+    trap = IntegerField('trap', validators=[DataRequired()], default=0)
+    climb = SelectField('climb', validators=[DataRequired()], choices=[
+        (0, 'None'),
+        (1, 'Single'),
+        (2, 'Double'),
+        (3, 'Triple'),
+    ])
+    spotlight = BooleanField('spotlight', default=False)
+
+    droppedPieces = IntegerField('Dropped Pieces', validators=[DataRequired()])
+
+    # Other info
+    intake = RadioField('intake', validators=[DataRequired()], choices=[
+        (0, 'Intake did not work'),
+        (1, 'Human player intake'),
+        (2, 'Ground intake'),
+        (3, 'Both'),
+    ])
+    defense = RadioField('defense', validators=[DataRequired()], choices=[
+        ('none', 'Did not play defense'),
+        ('poor', 'Poor defense'),
+        ('average', 'Average defense'),
+        ('excellent', 'Excellent defense')
+    ])
 
     info = TextAreaField('info', validators=[DataRequired()])
